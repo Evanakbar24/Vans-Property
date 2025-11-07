c0da70f906e5b6d5ba462806bd5f744e847f7ed5
Evan akbarudin <evanakbarudin088@gmail.com>
2025-11-07 08:24:30 +0700

    update something

---
+++ script.js
@@ -0,0 +1,44 @@
+// var angka = prompt('Masukan angka anda');
+
+// while(angka){
+//         if ( angka >= 90){
+//             alert('Nilai kamu A , bagus sekali pertahankan ya')
+//             break;
+//         } else if ( angka >= 80){
+//             alert('Nilai kamu B , bagus sekali pertahankan ya')
+//             break;
+//         } else if ( angka >= 70){
+//             alert('Nilai kamu C , kurang bagus tingkatkan lagi ya')
+//             break;
+//         } else {
+//             alert('Nilai kamu sangat kurang bagus tolong maksimalkan ')
+//         }
+//         break;
+// }
+
+// alert('Program selesai');
+
+
+
+
+// // while(confirm('Hello World')){
+
+// // }
+
+
+// for ( var i = 1 ; i <= 10; i++){
+//     var bintang = " "
+//  for ( var j = 1 ; j <= i ; j++){
+//     bintang +="*"
+//     }
+// console.log(bintang);
+// }
+
+// for ( var i = 10 ; i >= 1; i--){
+//     var bintang = " "
+//  for ( var j = 1 ; j < i ; j++){
+//     bintang +="*"
+//     }
+// console.log(bintang);
+// }
+
